83c02dfd8d7ed431c4a9d88f529584362f8969af
Shery Ann Manrique <sheryannmanrique37@gmail.com>
2026-09-14 00:21:42 +0800

    database

--- app/config/database.php
+++ app/config/database.php
@@ -58,17 +58,13 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 
 $database['main'] = array(
-    'driver'	=> getenv('DB_DRIVER') ?: '',
+    'driver'	=> getenv('DB_DRIVER') ?: 'mysql',
     'hostname'	=> getenv('DB_HOST') ?: '',
     'port'		=> getenv('DB_PORT') ?: '',
-    'username'	=> getenv('DB_USER') ?: '',
+    'username'	=> getenv('DB_USER') ?: 'avnadmin',
     'password'	=> getenv('DB_PASSWORD') ?: '',
     'database'	=> getenv('DB_NAME') ?: '',
-<<<<<<< HEAD
-    'charset'	=> getenv('DB_CHARSET') ?: 'utf8m4',
-=======
-    'charset'	=> getenv('DB_CHARSET') ?: '',
->>>>>>> 9014cf9687cef228c46dc99942ec380851890c5d
+    'charset'	=> getenv('DB_CHARSET') ?: 'utf8mb4',
     'dbprefix'	=> getenv('DB_PREFIX') ?: '',
     // Optional for SQLite
     'path'      => ''
