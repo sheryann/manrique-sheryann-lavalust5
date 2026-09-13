0c0e99feee3c669c42b097d5922d40a3bf20c830
Shery Ann Manrique <sheryannmanrique37@gmail.com>
2026-09-13 22:25:30 +0800

    Update routes

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,82 +1,37 @@
 <?php
+
 defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
-/**
- * ------------------------------------------------------------------
- * LavaLust - an opensource lightweight PHP MVC Framework
- * ------------------------------------------------------------------
- *
- * MIT License
- *
- * Copyright (c) 2020 Ronald M. Marasigan
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * @package LavaLust
- * @author Ronald M. Marasigan <ronald.marasigan@yahoo.com>
- * @since Version 1
- * @link https://github.com/ronmarasigan/LavaLust
- * @license https://opensource.org/licenses/MIT MIT License
- */
 
-/*
-| -------------------------------------------------------------------
-| URI ROUTING
-| -------------------------------------------------------------------
-| Here is where you can register web routes for your application.
-|
-|
-*/
-/** @var object $router **/
 
-//$router->get('/', 'Welcome::index');
+$router->any('/', 'AuthController::login');
+$router->any('/login', 'AuthController::login');
 
 
 
 
-$router->get('/', 'AuthController::login');
+$router->get('/products', 'ProductController::index')->middleware('auth');
 
-$router->get('/login', 'AuthController::login');
+$router->any('/products/create', 'ProductController::create')->middleware('auth');
 
-$router->post('/authenticate', 'AuthController::authenticate');
+$router->any('/products/edit/{id}', 'ProductController::edit')->middleware('auth');
 
-$router->get('/logout', 'AuthController::logout');
+$router->any('/products/delete/{id}', 'ProductController::delete')->middleware('auth');
 
 
-// Product routes with authentication middleware
 
-$router->get('/products', 'ProductController::index')
-       ->middleware('auth');
+$router->get('/logout', function()
+{
+    $_SESSION = array();
 
-$router->get('/products/create', 'ProductController::create')
-       ->middleware('auth');
+    session_destroy();
 
-$router->post('/products/store', 'ProductController::store')
-       ->middleware('auth');
+    redirect('login');
+    exit;
+});
 
-$router->get('/products/edit/{id}', 'ProductController::edit')
-       ->middleware('auth');
+$router->get('/not-logged-in', function()
+{
+    echo 'Please Login First.';
+});
 
-$router->post('/products/update/{id}', 'ProductController::update')
-       ->middleware('auth');
-
-$router->get('/products/delete/{id}', 'ProductController::delete')
-       ->middleware('auth');
-
-$router->post('/products/destroy/{id}', 'ProductController::destroy')
-       ->middleware('auth');
+?>
